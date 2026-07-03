3d30ed2186adf488085cb8737e5ef341766585c7
appOlivillaTres <appolivillatres@gmail.com>
2026-07-03 08:37:17 +0200

    facturassync

--- listar_carpetas.py
+++ listar_carpetas.py
@@ -2,9 +2,22 @@ import win32com.client
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
+facturas = None
+
 for i in range(1, outlook.Folders.Count + 1):
     cuenta = outlook.Folders.Item(i)
-    print(f"\n📧 Cuenta: {cuenta.Name}")
 
     for carpeta in cuenta.Folders:
-        print("   └──", carpeta.Name)
+        if carpeta.Name.upper() == "FACTURAS":
+            facturas = carpeta
+            break
+
+    if facturas:
+        break
+
+if facturas is None:
+    print("No se encontró la carpeta FACTURAS")
+    exit()
+
+print("Carpeta encontrada:", facturas.Name)
+print("Correos:", facturas.Items.Count)
